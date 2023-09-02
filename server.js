bd5aaf9048f89eb92728bbbf8664a975a6ce13fd
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-02 11:43:05 -0400

    temporarily removed some session elements, get reaches root, but loops infinitely

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');  // TODO - must reenable
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);  // TODO - must reenable
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
+console.log('got this far in server.js - #1');
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// const sess = {  // TODO - must reenable
+//   secret: 'Super secret secret',
+//   cookie: {
+//     maxAge: 300000,
+//     httpOnly: true,
+//     secure: false,
+//     sameSite: 'strict',
+//   },
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 
-app.use(session(sess));
+// app.use(session(sess));  // TODO - temporarily disabled, must restore and test
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
@@ -39,7 +40,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+console.log('got this far in server.js - #2');
+
 app.use(routes);
+
+console.log('got this far in server.js - #3');
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
