452b82abb9c9b9c09bf4dbb9acf0e8a51f213621
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-05 05:49:51 -0400

    deletion of blogpost working

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -99,5 +99,21 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!blogPostData) {
+      res.status(404).json({ message: 'No such blogpost id' });
+    } else {
+      res.status(200).json(blogPostData);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
