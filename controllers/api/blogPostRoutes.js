33b5d05448bfaff3ba768c6c90e199d2ee63cc7d
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-04 20:46:46 -0400

    get all for blogposts, but nested included will yet be attempted

---
+++ controllers/api/blogPostRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const { BlogPost, User, Comment } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.findAll({
+        include: [
+            {
+                model: User,
+                required: false
+            },
+            {
+                model: Comment,
+                required: false
+            }
+        ]
+    });
+    if (!blogPostData) {
+      res.status(400).json({ message: 'no posts retrieved' });
+    } else {
+      res.status(200).json(blogPostData);
+    }
+  } catch (err) {
+    res.status(401).json(err);
+  };
+}); 
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
+const blogPostRoutes = require('./blogPostRoutes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+router.use('/blogposts', blogPostRoutes);
 
 module.exports = router;
