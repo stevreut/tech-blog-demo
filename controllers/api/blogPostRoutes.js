795f2647a655edcce5abda038d3d081d2540831c
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-04 22:22:50 -0400

    post of blogpost working

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -31,4 +31,53 @@ router.get('/', async (req, res) => {
   };
 }); 
 
+router.get('/:id', async (req, res) => {
+  try {
+    console.log('get post id = "' + req.params.id + '"');
+    const blogPostData = await BlogPost.findByPk(req.params.id, {
+        include: [
+            {
+                model: User,
+                required: false
+            },
+            {
+                model: Comment,
+                include: [
+                    {
+                        model: User
+                    }
+                ],
+                required: false
+            }
+        ]
+    });
+    if (!blogPostData) {
+      res.status(404).json({ message: 'no post retrieved' });
+    } else {
+      res.status(200).json(blogPostData);
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  };
+}); 
+
+router.post('/', async (req, res) => {
+  try {
+    const { title, user_id, content } = req.body;
+    const creationDate = new Date();
+    const blogPostObj = {
+      title: title,
+      user_id: user_id,
+      creationDate: creationDate,
+      content: content
+    }
+    const blogPostData = await BlogPost.create(blogPostObj);
+    res.status(200).json(blogPostObj);
+  } catch (err) {
+    res.status(400).message(err);
+  }
+
+})
+
+
 module.exports = router;
